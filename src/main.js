50ed12ab9fd40a3bd61cee7d49cf861a4ced886a
ScrawnyViking <info@tbc.exchange>
2021-09-20 17:59:11 -0500

    https://tbc.exchange/#nft Buy a Bee NFT!

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require("fs"); // We Love HASHLIPS!!!
 const path = require("path");
 const sha1 = require("sha1");
 const { createCanvas, loadImage } = require("canvas");
